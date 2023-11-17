ecbfb07ebf4c1deef1a60ea9a2e0851d942c71c2
ahnsv <ahnsv@bc.edu>
2023-11-17 23:35:12 +0900

    feat(convert_to_dbt_jinja): add in-place option

--- dbttoolkit/core/io.py
+++ dbttoolkit/core/io.py
@@ -9,3 +9,9 @@ def read_sql_file(file: str | Path):
     with open(file) as f:
         input_data = f.read()
     return input_data
+
+
+def write_sql_file(file: str | Path, new_sql_content: str):
+    with open(file, "w") as f:
+        f.write(new_sql_content)
+    return new_sql_content
