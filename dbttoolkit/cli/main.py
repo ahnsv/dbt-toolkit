3343a2ea7452faa30ca47fac32f871933136a28a
ahnsv <ahnsv@bc.edu>
2023-11-17 23:45:21 +0900

    feat: add force mode

--- dbttoolkit/cli/main.py
+++ dbttoolkit/cli/main.py
@@ -40,10 +40,15 @@ class dbtSourceNodeSet:
 
 @app.command(help="convert a plain sql to dbt jinja sql, replacing existing source node with dbt expression")
 def convert_to_dbt_jinja(
-    sql_file_path: str, dbt_project_path: Path, in_place: Annotated[bool, typer.Option()] = False
+    sql_file_path: str,
+    dbt_project_path: Path,
+    in_place: Annotated[bool, typer.Option()] = False,
+    force: Annotated[bool, typer.Option("--force", "-f")] = False,
 ):
     sql = read_sql_file(sql_file_path)
     table_refs = extract_table_references(sql_query=sql)
+
+    is_force_mode = force is True
 
     manifest_json_path = dbt_project_path / "target/manifest.json"
     if not manifest_json_path.exists():
@@ -54,16 +59,20 @@ def convert_to_dbt_jinja(
     source_node_set = dbtSourceNodeSet(
         manifest_json["sources"], project_name=manifest_json["metadata"]["project_name"]
     )
+    number_of_changes = 0
     for table_ref in table_refs:
         project, dataset, table = table_ref.value.replace("`", "").split(".")
         source_node = source_node_set.search_by_schema_and_table_name(schema=dataset, table_name=table)
         if not source_node:
-            should_add = typer.confirm(f"[Add] {table_ref.value} is not existing in manifest.json. Should we add it?")
+            should_add = is_force_mode or typer.confirm(
+                f"[Add] {table_ref.value} is not existing in manifest.json. Should we add it?"
+            )
             if should_add:
                 # TODO: add
                 ...
+            number_of_changes += 1
             continue
-        should_replace = typer.confirm(
+        should_replace = is_force_mode or typer.confirm(
             f"[Replace] {table_ref.value} ➡️ {{{{ source('{source_node.schema}', '{source_node.identifier}') }}}}. Is it OK?"
         )
         if should_replace:
@@ -71,13 +80,14 @@ def convert_to_dbt_jinja(
                 table_ref.value,
                 f"{{{{ source('{source_node.schema}', '{source_node.identifier}') }}}}",
             )
+            number_of_changes += 1
     if not in_place:
         print(sql)
         return
 
     # write sql file
     write_sql_file(file=sql_file_path, new_sql_content=sql)
-    print(f"Changes have been made in [{sql_file_path}]")
+    print(f"{number_of_changes} changes have been made in [{Path(sql_file_path).absolute()}]")
 
 
 @app.command(
